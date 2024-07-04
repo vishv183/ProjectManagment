03164ee67da1e0ba4bc01b9f622e37bc68c488fc
vishv183 <vishvmunjapara23@gmail.com>
2024-07-04 12:14:08 +0530

    created model - Project, task

--- apps/Projects/admin.py
+++ apps/Projects/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from apps.Projects.models import Project, Task
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
--- apps/Projects/models.py
+++ apps/Projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    due_date = models.DateField(null=True, blank=True)
+
+class Task(models.Model):
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    due_date = models.DateField(null=True, blank=True)
